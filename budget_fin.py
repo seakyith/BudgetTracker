b73405eb2564d002c2f4003670cb72808055b23e
Shane <shazelquis@student.rccd.edu>
2021-11-29 19:27:51 -0800

    Line plot of budget against time of transactions

--- budget_fin.py
+++ budget_fin.py
@@ -2,6 +2,7 @@ import csv
 import sqlite3
 import matplotlib.pyplot as plt
 import numpy as np
+from datetime import datetime
 
 
 
@@ -38,6 +39,46 @@ for row in sale:
     amount.append(row[1])
 plt.pie(amount, labels=category)
 plt.show()
+
+# Line Graph of budget against time of transactions
+dates = [
+    i[0]
+    for i in cur.execute(
+        "SELECT DISTINCT dt.transDATE from date dt ORDER BY dt.transDATE"
+    )
+]
+dtime = []
+rbudg = [0]
+time_expense = {"Green": [[], []], "Red": [[], []]}
+
+for date in dates:
+    res = [
+        item[0]
+        for item in cur.execute(
+            "SELECT SUM(ba.balance) FROM balance ba, date dt WHERE dt.balance_id=ba.id AND dt.transDATE='{}'".format(
+                date
+            )
+        )
+    ]
+    dtime.append(datetime.strptime(date, "%m/%d/%Y"))
+    rbudg.append(rbudg[-1] + res[0])
+    if rbudg[-1] > 0:
+        time_expense["Green"][0].append(dtime[-1])
+        time_expense["Green"][1].append(rbudg[-1])
+    else:
+        time_expense["Red"][0].append(dtime[-1])
+        time_expense["Red"][1].append(rbudg[-1])
+
+del rbudg[0]
+# Apply data to plot
+plt.plot(dtime, rbudg, color="Blue", zorder=5)
+plt.scatter(
+    time_expense["Green"][0], time_expense["Green"][1], color="Green", zorder=10
+)
+plt.scatter(time_expense["Red"][0], time_expense["Red"][1], color="Red", zorder=15)
+plt.axhline(y=0, color="Black", linewidth=1, zorder=2)
+plt.show()
+
 # for i in category:
 #     print(i)
 # debitSum= cur.execute("SELECT SUM(balance) FROM balance where id IN (SELECT balance_id FROM Transtype where type = 'DEBIT')")
